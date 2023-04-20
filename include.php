93665ab1fb1e827ec6c51f1206bbd28f372d9b70
freedompay <cms@freedompay.money>
2023-04-20 11:12:22 +0000

    fix ci

---
+++ include.php
@@ -0,0 +1,4 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'] . BX_ROOT . '/php_interface/include/sale_payment/freedompay/FreedomPaySignature.php');
+include($_SERVER['DOCUMENT_ROOT'] . BX_ROOT . '/php_interface/include/sale_payment/freedompay/FreedomPayIO.php');
