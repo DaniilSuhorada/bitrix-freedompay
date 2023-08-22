c1e4bb13fd3f933339988e0cc7889a2fb52c257c
freedompay <cms@freedompay.money>
2023-08-22 08:26:28 +0000

    fix default api url

---
+++ lib/lib/FreedomPay.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once(__DIR__ . '/FreedomPay/ApiClient.php');
+require_once(__DIR__ . '/FreedomPay/Helper.php');
+require_once(__DIR__ . '/FreedomPay/Result.php');
+require_once(__DIR__ . '/FreedomPay/Payment.php');
